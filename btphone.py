496dcf63c10ed5df1c8e4e670eec3f297a3b1d6c
TheJostler <65976463+TheJostler@users.noreply.github.com>
2022-05-12 17:15:44 +0100

    Update btphone.py

--- btphone.py
+++ btphone.py
@@ -15,9 +15,7 @@ txt_red = fg('red')
 
 url = "https://www.thephonebook.bt.com/Person/PersonSearch"
 
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
-}
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'}
 
 def http_status(r):
 	if r.status_code == 200:
